180bb4ea665948252d01dcbe9c7ffda2f6cdbf83
telepatheticman <telepatheticman13@gmail.com>
2016-05-24 15:07:28 -0500

    started password generation

--- VQR/src/Encoding/passwordGneration.java
+++ VQR/src/Encoding/passwordGneration.java
@@ -1,5 +1,20 @@
 package Encoding;
 
-public class passwordGneration {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class passwordGneration {
+	
+	public static void main(String args[]){
+		Random rand = new Random();
+		List<Integer> parts = new ArrayList<Integer>(12);
+		int part;
+		String pass = null;
+		for (int x = 0; x < 12; x ++){
+			part = rand.nextInt(256);
+			parts.add(part);
+			System.out.println(parts.get(x));
+		}
+	}
 }
