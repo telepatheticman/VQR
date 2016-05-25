b66d4800339546a8711b1479f332b349870168fe
telepatheticman <telepatheticman13@gmail.com>
2016-05-25 15:29:33 -0500

    made thing that does stuff

--- VQR/src/Encoding/passwordEnkryption.java
+++ VQR/src/Encoding/passwordEnkryption.java
@@ -1,5 +1,16 @@
 package Encoding;
 
-public class passwordEnkryption {
+import java.util.ArrayList;
+import java.util.List;
 
+public class passwordEnkryption {
+	public static void main(String args[]){
+		String hexPass = passwordGeneration.Generation();
+		List<String> partsBin = new ArrayList<String>(12);
+		List<String> key = new ArrayList<String>(12);
+		for (int x = 0; x < 12; x++){
+			
+		}
+		System.out.println(hexPass);
+	}
 }
