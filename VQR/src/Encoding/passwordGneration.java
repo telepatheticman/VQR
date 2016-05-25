7781a0208c6874a48d4ad7d35c8d91b0b1e601f5
telepatheticman <telepatheticman13@gmail.com>
2016-05-24 22:10:53 -1000

    Added binary password creation.

--- VQR/src/Encoding/passwordGneration.java
+++ VQR/src/Encoding/passwordGneration.java
@@ -4,17 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
 public class passwordGneration {
 	
 	public static void main(String args[]){
 		Random rand = new Random();
-		List<Integer> parts = new ArrayList<Integer>(12);
+		List<String> partsBin = new ArrayList<String>(12);
 		int part;
+		int listTemp = 0;
+		String unpadded = null;
+		String partString = null;
+		String partStringtemp = null;
 		String pass = null;
 		for (int x = 0; x < 12; x ++){
 			part = rand.nextInt(256);
-			parts.add(part);
-			System.out.println(parts.get(x));
+			unpadded = Integer.toBinaryString(part);
+			partString = "00000000".substring(unpadded.length()) + unpadded;
+			partsBin.add(partString);
+			System.out.println(x + 1 + ". " + partsBin.get(x));
 		}
 	}
 }
