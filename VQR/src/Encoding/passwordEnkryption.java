5914f6ca47859684616e2e44cddd613254efe271
telepatheticman <telepatheticman13@gmail.com>
2016-05-25 22:32:03 -1000

    Encryption added

--- VQR/src/Encoding/passwordEnkryption.java
+++ VQR/src/Encoding/passwordEnkryption.java
@@ -6,11 +6,47 @@ import java.util.List;
 public class passwordEnkryption {
 	public static void main(String args[]){
 		String hexPass = passwordGeneration.Generation();
-		List<String> partsBin = new ArrayList<String>(12);
-		List<String> key = new ArrayList<String>(12);
-		for (int x = 0; x < 12; x++){
-			
+		String unpadBin;
+		String padBin;
+		String in;
+		String keyTemp;
+		String out = "";
+		List<String> partsBin = new ArrayList<String>(11);
+		List<String> key = new ArrayList<String>(11);
+		List<String> Encryption = new ArrayList<String>(11);
+		key.add("10011010");
+		key.add("10101010");
+		key.add("01010101");
+		key.add("11001100");
+		key.add("10110100");
+		key.add("01000111");
+		key.add("01011100");
+		key.add("11001101");
+		key.add("11011011");
+		key.add("11001111");
+		key.add("01001001");
+		for (int x = 0; x < 11; x++){
+			unpadBin = Integer.toBinaryString(Integer.parseInt(hexPass.substring(2*x, 2*x + 2), 16));
+			padBin = "00000000".substring(unpadBin.length()) + unpadBin;
+			partsBin.add(padBin);
+//			System.out.println(partsBin.get(x));
 		}
-		System.out.println(hexPass);
+//		System.out.println(hexPass);
+		for (int y = 0; y < 11; y++){
+			in = partsBin.get(y);
+			keyTemp = key.get(y);
+			out = "";
+			for (int z = 0; z < 8; z++){
+				if ((in.substring(z, z + 1).equals("0") && keyTemp.substring(z, z + 1).equals("1"))
+				  ||(in.substring(z, z + 1).equals("1") && keyTemp.substring(z, z + 1).equals("0"))){
+					out = out + "1";
+				}
+				else{
+					out = out + "0";
+				}
+			}
+			Encryption.add(out);
+			System.out.println(partsBin.get(y) + "\n" + key.get(y) + "\n" + Encryption.get(y) + "\n");
+		}
 	}
 }
--- VQR/src/Encoding/passwordGeneration.java
+++ VQR/src/Encoding/passwordGeneration.java
@@ -1,7 +1,6 @@
 package Encoding;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Random;
 
 
@@ -9,20 +8,14 @@ public class passwordGeneration {
 	
 	public static String Generation(){
 		Random rand = new Random();
-		List<String> partsBin = new ArrayList<String>(12);
 		String hexPass = null;
 		int part;
-		String unpaddedBin = null;
 		String unpaddedHex = null;
-		String partStringBin = null;
 		String partStringHex = null;
 		for (int x = 0; x < 11; x ++){
 			part = rand.nextInt(256);
-			unpaddedBin = Integer.toBinaryString(part);
 			unpaddedHex = Integer.toHexString(part);
-			partStringBin = "00000000".substring(unpaddedBin.length()) + unpaddedBin;
 			partStringHex = "00".substring(unpaddedHex.length()) + unpaddedHex;
-			partsBin.add(partStringBin);
 			hexPass = partStringHex + hexPass;
 		//	System.out.println(x + 1 + ". " + partsBin.get(x));
 		}
