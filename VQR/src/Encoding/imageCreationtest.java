7b1014dcaa0014526df749c860eac92e8e914730
telepatheticman <telepatheticman13@gmail.com>
2016-08-20 11:21:41 -0600

    images in new location SHOULD create a new image from other images Documentation still crap everywhere

--- VQR/src/Encoding/imageCreationtest.java
+++ VQR/src/Encoding/imageCreationtest.java
@@ -1,5 +1,26 @@
 package Encoding;
 
-public class imageCreationtest {
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 
+public class imageCreationtest {
+	public void paint(Graphics g) throws IOException{
+	//	File path = new File("\\VQR\\Images");
+	//	String path = "\\VQR\\Images";
+		
+			BufferedImage A = ImageIO.read(new File("A.jpg"));
+			BufferedImage B = ImageIO.read(new File("B.jpg"));
+		
+		BufferedImage AB = new BufferedImage(100, 50, BufferedImage.TYPE_INT_ARGB);
+	/*	Graphics g */ AB.getGraphics();
+		g.drawImage(A, 0, 0, null);
+		g.drawImage(B, 50, 0, null);
+
+		// Save as new image
+		ImageIO.write(AB, "JPG", new File("AB.jpg"));
+	}
+	
 }
